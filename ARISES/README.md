63b5dad15a80ed76796bd8226c4bc1f653bef79c
Ryan Armiger <ryanarmiger@dyn3201-68.wlan.ic.ac.uk>
2018-05-07 17:49:26 +0100

    Basic expanding for two tabs with unfinished autolayout

--- ARISES/README.md
+++ ARISES/README.md
@@ -6,9 +6,15 @@
 - None
 ### View 
 - Backgrounds for each section
-- Titles for each section
-- Symbols for each section
+- 4 expanded views
+- 4 contracted views
+- *contracted views have "states" and any widgets move to be visible based on those*
+- Set each view to be "connected" so only a few variables to adjust
+- Titles for each view
+- Symbols for each view
 ### Controller
-- Buttons to select section
-- Actions to adjust section size
-- Make "options" appear/disappear
+- Buttons to select Views (expand/contract)
+- Actions to exand/contract
+- *ensure only one view expanded at a time*
+- AutoLayout for all
+
--- ARISES/ViewController.swift
+++ ARISES/ViewController.swift
@@ -11,47 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK: Properties
-    @IBOutlet weak var ColourBox: UIButton!
-    
-    @IBOutlet weak var Button1: UIButton!
-    @IBOutlet weak var Button2: UIButton!
-    @IBOutlet weak var Button3: UIButton!
-    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var viewExpandedFood: UIView!
+    @IBOutlet weak var viewExpandedHealth: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     //MARK: Actions
-    @IBAction func ColourButton1(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.9882352941, green: 0.1411764706, blue: 0.8823529412, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.9882352941, green: 0.1411764706, blue: 0.8823529412, alpha: 1)
-        }
-        
+    @IBAction func ExpandFood(_ sender: UIButton) {
+        viewExpandedFood.isHidden = false
     }
-    @IBAction func ColourButton2(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.05098039216, green: 0.2235294118, blue: 0.9843137255, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.05098039216, green: 0.2235294118, blue: 0.9843137255, alpha: 1)
-        }
+    @IBAction func ContractFood(_ sender: UIButton) {
+        viewExpandedFood.isHidden = true
     }
-    @IBAction func ColourButton3(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.1725490196, green: 0.9921568627, blue: 0.1803921569, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.9921568627, blue: 0.1803921569, alpha: 1)
-        }
+    @IBAction func ExpandHealth(_ sender: UIButton) {
+        viewExpandedHealth.isHidden = false
     }
-    @IBAction func ColourButton4(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.9921568627, green: 0.5254901961, blue: 0.1411764706, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.5254901961, blue: 0.1411764706, alpha: 1)
-        }
+    @IBAction func ContractHealth(_ sender: UIButton) {
+        viewExpandedHealth.isHidden = true
     }
+    
 }
-
-
