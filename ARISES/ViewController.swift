328c2658a5c6d88d0ad4edf6cd0e717b231d759d
Ryan Armiger <ryanarmiger@dyn3205-19.wlan.ic.ac.uk>
2018-05-08 16:23:45 +0100

    all buttons

--- ARISES/ViewController.swift
+++ ARISES/ViewController.swift
@@ -13,23 +13,56 @@ class ViewController: UIViewController {
     //MARK: Properties
     @IBOutlet weak var viewExpandedFood: UIView!
     @IBOutlet weak var viewExpandedHealth: UIView!
+    @IBOutlet weak var viewExpandedExercise: UIView!
+    @IBOutlet weak var viewExpandedAdvice: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     //MARK: Actions
-    @IBAction func ExpandFood(_ sender: UIButton) {
+
+    @IBAction func smallFoodButton(_ sender: UIButton) {
         viewExpandedFood.isHidden = false
+        viewExpandedHealth.isHidden = true
+        viewExpandedExercise.isHidden = true
+        viewExpandedAdvice.isHidden = true
     }
-    @IBAction func ContractFood(_ sender: UIButton) {
+    
+    @IBAction func smallHealthButton(_ sender: UIButton) {
+        viewExpandedFood.isHidden = true
+        viewExpandedHealth.isHidden = false
+        viewExpandedExercise.isHidden = true
+        viewExpandedAdvice.isHidden = true
+    }
+    
+    @IBAction func smallExerciseButton(_ sender: UIButton) {
+        viewExpandedFood.isHidden = true
+        viewExpandedHealth.isHidden = true
+        viewExpandedExercise.isHidden = false
+        viewExpandedAdvice.isHidden = true
+    }
+    
+    @IBAction func smallAdviceButton(_ sender: UIButton) {
+        viewExpandedFood.isHidden = true
+        viewExpandedHealth.isHidden = true
+        viewExpandedExercise.isHidden = true
+        viewExpandedAdvice.isHidden = false
+    }
+    
+    @IBAction func collapseFood(_ sender: UIButton){
         viewExpandedFood.isHidden = true
     }
-    @IBAction func ExpandHealth(_ sender: UIButton) {
-        viewExpandedHealth.isHidden = false
+    @IBAction func collapseAdvice(_ sender: UIButton) {
+        viewExpandedAdvice.isHidden = true
     }
-    @IBAction func ContractHealth(_ sender: UIButton) {
+    @IBAction func collapseExercise(_ sender: UIButton) {
+        viewExpandedExercise.isHidden = true
+    }
+    @IBAction func collapseHealth(_ sender: UIButton) {
         viewExpandedHealth.isHidden = true
     }
+    
+    
     
 }
