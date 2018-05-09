3ef4f8b4614a42201809208abc3f7a580ff93c9d
Ryan Armiger <ryanarmiger@dyn3192-66.wlan.ic.ac.uk>
2018-05-09 12:03:33 +0100

    Clean Slate

--- ARISES/ViewController.swift
+++ ARISES/ViewController.swift
@@ -11,47 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK: Properties
-    @IBOutlet weak var ColourBox: UIButton!
-    
-    @IBOutlet weak var Button1: UIButton!
-    @IBOutlet weak var Button2: UIButton!
-    @IBOutlet weak var Button3: UIButton!
-    @IBOutlet weak var Button4: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     //MARK: Actions
-    @IBAction func ColourButton1(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.9882352941, green: 0.1411764706, blue: 0.8823529412, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.9882352941, green: 0.1411764706, blue: 0.8823529412, alpha: 1)
-        }
-        
-    }
-    @IBAction func ColourButton2(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.05098039216, green: 0.2235294118, blue: 0.9843137255, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.05098039216, green: 0.2235294118, blue: 0.9843137255, alpha: 1)
-        }
-    }
-    @IBAction func ColourButton3(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.1725490196, green: 0.9921568627, blue: 0.1803921569, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.9921568627, blue: 0.1803921569, alpha: 1)
-        }
-    }
-    @IBAction func ColourButton4(_ sender: UIButton) {
-        if ColourBox.backgroundColor == #colorLiteral(red: 0.9921568627, green: 0.5254901961, blue: 0.1411764706, alpha: 1) {
-            ColourBox.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else {
-            ColourBox.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.5254901961, blue: 0.1411764706, alpha: 1)
-        }
-    }
 }
 
 
