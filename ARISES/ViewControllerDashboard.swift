bd52b5962e4bdd3c20eaa7d8ccc5ea0321c125a4
Ryan Armiger <ryanarmiger@Peters-MacBook-Pro.local>
2018-05-13 17:32:29 +0100

    dahboard basic

---
+++ ARISES/ViewControllerDashboard.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerDashboard.swift
+//  ARISES
+//
+//  Created by Ryan Armiger on 13/05/2018.
+//  Copyright © 2018 Ryan Armiger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerDashboard: UIViewController {
+    
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Actions
+}
