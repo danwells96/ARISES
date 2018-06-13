bdb239e87fb5faeb9d0a7ea4773539fd832baadf
Ryan Armiger <ryanarmiger@dyn3202-128.wlan.ic.ac.uk>
2018-06-13 17:52:15 +0100

    minor layout changes

--- ARISES/Supporting Files/AppDelegate.swift
+++ ARISES/Supporting Files/AppDelegate.swift
@@ -20,11 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-/*
+
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait.union(UIInterfaceOrientationMask.portraitUpsideDown)
     }
- */
+ 
  
 
     func applicationWillResignActive(_ application: UIApplication) {
