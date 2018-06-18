21f76d1d8beafcf6fefa74554db9864411cb2a01
MXYs <chellemah@gmail.com>
2018-06-18 13:58:34 +0100

    Add files via upload

--- ARISES/Display/ChartBGView.swift
+++ ARISES/Display/ChartBGView.swift
@@ -11,23 +11,31 @@ import UIKit
 class ChartBGView: UIView {
     
     override func draw(_ rect: CGRect) {
-        // Drawing code
         let path = UIBezierPath(rect: bounds)
         path.addClip()
-        // color bands
+        
+        drawMiddleBand()
+        
+        drawTopBand()
+        
+    }
+    ///color 'good' range
+    private func drawMiddleBand(){
         let middleRect = CGRect(
             origin: CGPoint(x: 0.0, y: bounds.height/2),
             size: CGSize(width: frame.width, height: frame.height * 0.3)
         )
         #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1).set()
         UIRectFill(middleRect)
-        
+    }
+    
+    ///color high and low ranges
+    private func drawTopBand(){
         let topRect = CGRect(
             origin: bounds.origin,
             size: CGSize(width: frame.width, height: frame.height / 2))
         #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1).set()
         UIRectFill(topRect)
-        
     }
     
     
