dbb5da6de926bfd55d2325b704a96e37baf5cb49
Dan Wells <dtw14@ic.ac.uk>
2018-06-22 14:58:50 +0100

    SOme comments

--- ARISES/Display/CustomView.swift
+++ ARISES/Display/CustomView.swift
@@ -15,10 +15,21 @@ import UIKit
 */
 class CustomView: UIView {
     
+	//MARK: Member Variables
+	///CGFloat containing the maximum glucose measurement for that day.
     var dailyHigh: CGFloat
+	///CGFloat containing the minimum glucose measurement for that day.
     var dailyLow: CGFloat
+	///CGFloat containing the mean average of the glucose measurements for that day.
     var avgArrayValue: CGFloat
     
+	//MARK: Member Methods
+	/**
+		The init method is called on the instantiation of a CustomView. It initializes the member variables to 0 which is used to determine
+		if there is any data stored for this CustomView.
+		- Paramater frame: CGRect, the bounds of the View.
+		- Paramater dailyHigh: CGFloat, the maximum glucose reading for the day.
+	*/
     init(frame: CGRect, dailyHigh: CGFloat) {
         self.dailyHigh = 0
         self.avgArrayValue = 0
@@ -33,6 +44,12 @@ class CustomView: UIView {
         super.init(coder: aDecoder)
     }
     
+	/**
+		This is an override method from the superclass and is called when the CustomView is created by the ViewController. It calls
+		methods which draw the 'safe' region onto the view. It also sets the visibility of the CustomView depending on if the CustomView
+		has data stored in it.
+		- Paramater rect: CGRect, the bounds of the CustomView.
+	*/
     override func draw(_ rect: CGRect) {
 
         let path = UIBezierPath(rect: bounds)
@@ -42,17 +59,16 @@ class CustomView: UIView {
         
         colourMiddleBand()
         
-        if(!((self.dailyLow == 0) && (self.dailyHigh == 0) && (self.avgArrayValue == 0))){
-            
+		//If no data is stored (i.e the view is still initialized), makes the view invisible.
+        if(!((self.dailyLow == 0) && (self.dailyHigh == 0) && (self.avgArrayValue == 0))){           
             self.isHidden = false
             drawRangeBar()
         }else{
-            
             self.isHidden = true
         }
     }
     
-    ///fill BGColor for 'good' band
+    ///Creates a rectangle of colour White from the mid-point of the CustomView (~10 on graph to ~4) to create the 'safe' region.
     private func colourMiddleBand(){
         
         let middleRect = CGRect(
